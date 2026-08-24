38e304c800670dfc76191e7bfecfa7b1159e18a6
Ivan Romanov <drizt72@zoho.eu>
2026-08-24 15:33:23 +0500

    feat(windows): enforce single app instance

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -5,8 +5,45 @@
 #include "flutter_window.h"
 #include "utils.h"
 
+namespace {
+
+constexpr wchar_t kInstanceMutexName[] =
+    L"Local\\io.github.drizt.tasks_tracker_client";
+constexpr wchar_t kWindowClassName[] = L"FLUTTER_RUNNER_WIN32_WINDOW";
+constexpr wchar_t kWindowTitle[] = L"tasks_tracker_client";
+constexpr int kWindowLookupAttempts = 40;
+constexpr DWORD kWindowLookupDelayMs = 50;
+
+void ActivateExistingWindow() {
+  for (int attempt = 0; attempt < kWindowLookupAttempts; ++attempt) {
+    HWND window = ::FindWindow(kWindowClassName, kWindowTitle);
+    if (window != nullptr) {
+      const int show_command = ::IsIconic(window) ? SW_RESTORE : SW_SHOW;
+      ::ShowWindowAsync(window, show_command);
+      ::SetWindowPos(window, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+      ::SetForegroundWindow(window);
+      return;
+    }
+
+    ::Sleep(kWindowLookupDelayMs);
+  }
+}
+
+}  // namespace
+
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
+  HANDLE instance_mutex = ::CreateMutex(nullptr, TRUE, kInstanceMutexName);
+  if (instance_mutex == nullptr) {
+    return EXIT_FAILURE;
+  }
+
+  if (::GetLastError() == ERROR_ALREADY_EXISTS) {
+    ActivateExistingWindow();
+    ::CloseHandle(instance_mutex);
+    return EXIT_SUCCESS;
+  }
+
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
@@ -27,7 +64,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"tasks_tracker_client", origin, size)) {
+  if (!window.Create(kWindowTitle, origin, size)) {
+    ::CoUninitialize();
+    ::CloseHandle(instance_mutex);
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
@@ -39,5 +78,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   }
 
   ::CoUninitialize();
+  ::CloseHandle(instance_mutex);
   return EXIT_SUCCESS;
 }
